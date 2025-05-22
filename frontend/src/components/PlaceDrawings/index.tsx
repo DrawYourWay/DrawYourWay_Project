7e96c0e5801127c109aa3586c43122ab9eff2a43
MateuszMac <mateuszcodedeveloper@gmail.com>
2025-05-22 10:50:26 +0100

    Adding animation to update drawings

--- frontend/src/components/PlaceDrawings/index.tsx
+++ frontend/src/components/PlaceDrawings/index.tsx
@@ -1,6 +1,9 @@
 import { PlaceDrawingsUrls } from "@/types/places/place";
 import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+const MotionBox = motion.create(Box);
 
 interface PlaceDrawingsProps {
   placeId: string;
@@ -9,7 +12,8 @@ interface PlaceDrawingsProps {
 const PlaceDrawings = ({ placeId }: PlaceDrawingsProps) => {
   const placeWebsocketUrl = `${import.meta.env.VITE_WEBSOCKET_URL}/places/${placeId}/`;
 
-  const [placeWebsocket, setPlaceWebsocket] = useState<WebSocket | null>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, setPlaceWebsocket] = useState<WebSocket | null>(null);
   const wsRef = useRef<WebSocket | null>(null);
 
   const [drawings, setDrawings] = useState<(string | null)[]>([]);
@@ -72,23 +76,35 @@ const PlaceDrawings = ({ placeId }: PlaceDrawingsProps) => {
           border="solid 1px"
           borderColor="black"
         >
-          {drawing ? (
-            <Image
-              src={drawing}
-              alt={`Drawing ${drawing} for place ${placeId}`}
-              width="100%"
-              height="100%"
-              objectFit="contain"
-              background="white"
-            />
-          ) : (
-            <Box
-              width="100%"
-              height="100%"
-              background="gray.100"
-              borderRadius="md"
-            />
-          )}
+          <MotionBox
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.4 }}
+            width="100%"
+            height="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            {drawing ? (
+              <Image
+                src={drawing}
+                alt={`Drawing ${drawing} for place ${placeId}`}
+                width="100%"
+                height="100%"
+                objectFit="contain"
+                background="white"
+              />
+            ) : (
+              <Box
+                width="100%"
+                height="100%"
+                background="gray.100"
+                borderRadius="md"
+              />
+            )}
+          </MotionBox>
         </GridItem>
       ))}
     </Grid>
